e217d823cca2e2df25fd79b8a2aabf9fdaf78481
Bug <brumbus32@protonmail.com>
2025-01-27 17:47:37 +1100

    coins

--- week-02/Scripts/character_controller.gd
+++ week-02/Scripts/character_controller.gd
@@ -25,7 +25,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(60))
 	
 	if event is InputEventKey and Input.is_action_pressed("ui_end"):
 		get_tree().quit()
---
+++ week-02/Scripts/interactable_coin.gd
@@ -0,0 +1,4 @@
+extends Interactable
+
+#todo: load different coin model depending on value. on interact, add to total and delete self and play sounds
+@export var value: int
